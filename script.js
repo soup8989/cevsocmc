7e93df561066080722f3a0b6e07ce39b11fe884c
CATON <catontan7@gmail.com>
2026-02-19 01:03:46 +1100

    feat: tab switching appears to be working!

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const buttons = document.querySelectorAll(".tab-btn");
-const contents = document.querySelector(".content");
+const contents = document.querySelectorAll(".content");
+
+console.log("JS");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+        console.log("clicked");
         buttons.forEach(btn => btn.classList.remove("active"));
-        buttons.forEach(content => content.classList.remove("active"));
-        button.classList.add("active");
+        contents.forEach(content => content.classList.remove("active"));
 
         const tab = button.dataset.tab;
         document.getElementById(tab).classList.add("active");
