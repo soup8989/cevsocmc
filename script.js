f396adbed7e2b0d57ccfb5ecdebfe09e933f1137
Caton Tan <z5478613@ad.unsw.edu.au>
2026-02-18 20:07:07 +1100

    added script file to sync with desktop

---
+++ script.js
@@ -0,0 +1,11 @@
+function openTab(evt, tabName) {
+    var i, tabContent, tabLinks
+
+    tabContent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabLinks)
+}
