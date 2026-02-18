46d491e3fd4ae2143e1f5f6204d01c5a046d8ec7
CATON <catontan7@gmail.com>
2026-02-19 10:04:56 +1100

    feat: home content

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const buttons = document.querySelectorAll(".tab-btn");
 const contents = document.querySelectorAll(".content");
 
-console.log("JS");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        console.log("clicked");
         buttons.forEach(btn => btn.classList.remove("active"));
         contents.forEach(content => content.classList.remove("active"));
 
